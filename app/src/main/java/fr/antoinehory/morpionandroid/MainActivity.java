d35c5b037d45ab0a2472daa223cf7c50e20a612b
Crock <contact@antoinehory.fr>
2023-10-25 15:41:34 +0200

    First commit with an empty activity

---
+++ app/src/main/java/fr/antoinehory/morpionandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.antoinehory.morpionandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
